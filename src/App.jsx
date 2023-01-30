e772b7eb970b12b4394f577ca0e9cf31c4d3e95a
yfrommelt <yoann@frommelt.fr>
2023-01-30 16:01:47 +0100

    :sparkles: controlled selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 
 function App() {
     const [userList, setUserList] = useState([])
+    const [userSelection, setUserSelection] = useState([])
 
     useEffect(() => {
         fetch('https://infallible-tereshkova-717266.netlify.app/.netlify/functions/server/users').then((res) => {
@@ -13,13 +14,30 @@ function App() {
         })
     }, [])
 
+    const toggleItem = (array, item) => {
+        return array.includes(item)
+            ? array.filter((user) => user !== item)
+            : array.concat(item);
+    }
+    const handleInput = (event) => {
+        setUserSelection(toggleItem(userSelection, Number(event.target.value)))
+    }
+
     return (
         <>
             <h1 className="title">Average age calculator</h1>
             <div className="user-list">
                 <ul>
                     {userList.map((user) => (
-                        <li key={user.id}><label><input type="checkbox" />{user.name}</label></li>
+                        <li key={user.id}>
+                            <label>
+                                <input type="checkbox"
+                                       value={user.id}
+                                       checked={userSelection.includes(user.id)}
+                                       onChange={handleInput} />
+                                {user.name}
+                            </label>
+                        </li>
                     ))}
                 </ul>
             </div>
